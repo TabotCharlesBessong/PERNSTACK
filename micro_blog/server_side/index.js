84bb6de2d02286147191e00fa04bb61c7795a06c
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-05-11 18:36:26 +0100

    setting up routes

--- micro_blog/server_side/index.js
+++ micro_blog/server_side/index.js
@@ -1,2 +1,38 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const session = require('express-session')
+const passport = require('passport')
+const indexRouter = require('./routes/indexRouter')
+const authRouter = require('./routes/authRouter')
 
-console.log("Hello world !")
+
+const port = process.env.PORT
+const app = express()
+
+// middlewares
+app.use(cors({
+  credentials:true,
+  origin:process.env.CLIENT_URL
+}))
+
+app.use(session({
+  secret:process.env.COOKIE_SECRET,
+  cookie:{
+    secure:process.env.NODE_ENV === 'production' ? 'true' : 'auto',
+    sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+  },
+  resave:false,
+  saveUninitialized:false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session)
+
+// routes
+app.use('/',indexRouter)
+app.use('/auth',authRouter)
+
+app.listen(port, ()=> {
+  console.log(`Server is running on port number ${port}...`)
+})
