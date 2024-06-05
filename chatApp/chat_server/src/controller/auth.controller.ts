3306697bbd2728fd67dd19190186d23e26a55a01
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-06-05 17:17:00 +0100

    getting user profile

--- chatApp/chat_server/src/controller/auth.controller.ts
+++ chatApp/chat_server/src/controller/auth.controller.ts
@@ -102,3 +102,22 @@ export const logout = async (req: Request, res: Response) => {
   }
 };
 
+export const getMe = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id: req.user.id } });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({
+      id: user.id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error: any) {
+    console.log("Error in getMe controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- chatApp/chat_server/src/router/auth.router.ts
+++ chatApp/chat_server/src/router/auth.router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { getMe, login, logout, signup } from "../controller/auth.controller";
+import protectedRoute from "../middleware/protectedRoute";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.get("/me", getMe);
+router.get("/me", protectedRoute,getMe);
 
 export default router;
