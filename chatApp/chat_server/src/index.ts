113acac5a5951ce9ac812cd5a717dd7313836d70
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-06-05 14:56:35 +0100

    establishing connection to the database

--- chatApp/chat_server/src/index.ts
+++ chatApp/chat_server/src/index.ts
@@ -1,6 +1,17 @@
 import express from "express"
+import authRouter from "./router/auth.router"
+import messagesRouter from "./router/messages.router"
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
 
 const app = express()
+dotenv.config()
+
+app.use(cookieParser())
+app.use(express.json())
+
+app.use("/api/auth",authRouter)
+// app.use("/api/messages",messagesRouter)
 
 app.listen(5000,()=>{
   console.log("Server is running on port 5000....")
