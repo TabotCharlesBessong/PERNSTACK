3a5a6ff633c63c14680fcd0dd95e31e119d46811
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-06-07 16:48:10 +0100

    auth context and protection routing

--- chatApp/chat_frontend/src/App.tsx
+++ chatApp/chat_frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Home, Login, Signup } from "./pages";
+import { useAuthContext } from "./context/AuthContext";
 
 const App = () => {
+  const {authUser,isLoading} = useAuthContext()
+  if(isLoading) return <h1>Loading.....</h1>
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
