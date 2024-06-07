2dd0fa0e74886bb0fd8337e0d3a6695892095562
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-06-07 06:05:53 +0100

    sending messages

---
+++ chatApp/chat_server/src/controller/message.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import prisma from "../db/prisma";
+
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user.id;
+    let conversation = await prisma.conversation.findFirst({
+      where: { participantIds: { hasEvery: [senderId, receiverId] } },
+    });
+
+    if (!conversation)
+      conversation = await prisma.conversation.create({
+        data: { participantIds: { set: [senderId, receiverId] } },
+      });
+    const newMessage = await prisma.message.create({
+      data: { senderId, body: message, conversationId: conversation.id },
+    });
+    if (newMessage)
+      conversation = await prisma.conversation.update({
+        where: { id: conversation.id },
+        data: { messages: { connect: { id: newMessage.id } } },
+      });
+    res.status(201).json(newMessage);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- chatApp/chat_server/src/router/messages.router.ts
+++ chatApp/chat_server/src/router/messages.router.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import protectedRoute from '../middleware/protectedRoute';
+import { sendMessage } from '../controller/message.controller';
 const router = express.Router()
+
+router.post("/send/:id",protectedRoute,sendMessage)
 
 export default router
