f2ca85c75017151f8d79d844fca6a63950459a9d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-06-07 17:41:18 +0100

    login services

--- chatApp/chat_frontend/src/App.tsx
+++ chatApp/chat_frontend/src/App.tsx
@@ -1,18 +1,32 @@
-import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import { Home, Login, Signup } from "./pages";
 import { useAuthContext } from "./context/AuthContext";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const {authUser,isLoading} = useAuthContext()
-  if(isLoading) return <h1>Loading.....</h1>
+  const { authUser, isLoading } = useAuthContext();
+  if (isLoading) return <h1>Loading.....</h1>;
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Router>
         <Routes>
-          <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"} />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <Login /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <Signup /> : <Navigate to={"/"} />}
+          />
         </Routes>
         <Toaster />
       </Router>
--- chatApp/chat_frontend/src/pages/Login.tsx
+++ chatApp/chat_frontend/src/pages/Login.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const {loading,login} = useLogin()
+  const handleSubmitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    login(inputs.username, inputs.password);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -9,7 +20,7 @@ const Login = () => {
           <span className="text-blue-500"> ChatApp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmitForm}>
           <div>
             <label className="label p-2 ">
               <span className="text-base label-text">Username</span>
@@ -18,6 +29,10 @@ const Login = () => {
               type="text"
               placeholder="Enter username"
               className="w-full input input-bordered h-10"
+              value={inputs.username}
+              onChange={(e) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
             />
           </div>
 
@@ -29,6 +44,10 @@ const Login = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={inputs.password}
+              onChange={(e) =>
+                setInputs({ ...inputs, password: e.target.value })
+              }
             />
           </div>
           <Link
@@ -39,7 +58,9 @@ const Login = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2">Login</button>
+            <button className="btn btn-block btn-sm mt-2" disabled={loading}>
+              {loading ? "Loading..." : "Login"}
+            </button>
           </div>
         </form>
       </div>
