2cdb147c510128093d8cc6adfa065b9c51684c39
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-06-07 16:27:35 +0100

    created the auth pages

--- chatApp/chat_frontend/src/App.tsx
+++ chatApp/chat_frontend/src/App.tsx
@@ -1,19 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Login, Signup } from "./pages";
+import { Home, Login, Signup } from "./pages";
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <h1 className="text-center text-2xl text-orange-700">
-                Hello people
-              </h1>
-            }
-          />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
